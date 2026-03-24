bac2b0327241ae55b705f3c2e4a6b6ab4d49345d
AlessiaDP <18192355@itisrossi.vi.it>
2026-03-24 10:26:06 +0100

    commit1 calc

---
+++ operazioni.c
@@ -0,0 +1,17 @@
+//
+// Created by 18192355 on 24/03/2026.
+//
+
+#include "operazioni.h"
+
+// operazioni.h
+#ifndef OPERAZIONI_H
+#define OPERAZIONI_H
+int somma(int a, int b);
+int differenza(int a, int b);
+#endif
+// operazioni.c  (lasciare VUOTO per ora)
+#include "operazioni.h"
+int somma(int a, int b) {
+    return a + b;
+}
